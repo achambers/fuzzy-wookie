ae3aef3e5c534d0406570764e09a8cb5dc2e7b02
Aaron Chambers <achambers@gmail.com>
2014-07-31 18:07:38 +0100

    Now we retrieve the index and return it if available

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
+var redis = require('redis');
+var Q = require('q');
+var logger = require('morgan');
 var express = require('express')
 var app = express();
+var appName = process.env.APP_NAME || 'my-app';
+var fileName = process.env.FILE_NAME || 'index';
+var logFormat = process.env.LOG_FORMAT || 'short';
 
 app.set('port', (process.env.PORT || 5000));
-//app.use(express.static(__dirname + '/public'));
+
+app.use(logger(logFormat));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  var client = redis.createClient();
+  var manifestId = request.query['manifest_id'];
+  var key = appName + ':' + fileName + ':';
+  var content = null;
+
+  if (manifestId) {
+    key = key.concat(manifestId);
+  } else {
+    key = key.concat('current');
+  }
+
+  client.on('connect', function() {
+    console.log('Connected to redis');
+  });
+
+  client.on('error', function(error) {
+    console.log('Redis error: ' + error);
+  });
+
+  Q.ninvoke(client, 'get', key).then(function(html) {
+    content = html;
+  }, function(reason) {
+    console.log('Error retrieving data from Redis: ', reason);
+  }).finally(function() {
+    client.quit();
+
+    if (content) {
+      response.send(content);
+    } else {
+      response.status(404).send('Not Found');
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
