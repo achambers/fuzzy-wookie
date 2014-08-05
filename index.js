f44c10909f87a3296e944cd79266b279bf636db6
Aaron Chambers <achambers@gmail.com>
2014-08-05 17:48:36 +0100

    Moved redis logic into it's own promise aware file

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-var redis = require('redis');
-var RSVP = require('rsvp');
-var logger = require('morgan');
 var express = require('express')
+var logger = require('morgan');
 var app = express();
 var logFormat = process.env.LOG_FORMAT || 'short';
-var defaultRedisUrl = 'redis://127.0.0.1:6379';
-var client;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(logger(logFormat));
 
 app.get('/', function(request, response) {
+  var redis = require('./lib/redis')();
   var manifestId = request.query['manifest-id'] || 'current';
   var key = 'index:' + manifestId;
-  var content = null;
 
-  connect()
+  redis.connect()
   .then(function() {
-    return get(key);
+    return redis.get(key);
   })
   .then(function(data) {
     if (data) {
@@ -31,43 +27,10 @@ app.get('/', function(request, response) {
     console.log('Error retrieving data from Redis: ', reason);
   })
   .finally(function() {
-    client.quit();
+    redis.quit();
   });
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
-
-function connect() {
-  var redisUrl   = require("url").parse(process.env.REDISTOGO_URL || defaultRedisUrl);
-  client = require("redis").createClient(redisUrl.port, redisUrl.hostname);
-
-  if(redisUrl.auth){
-    client.auth(redisUrl.auth.split(":")[1]);
-  }
-
-  client.on('error', function(error) {
-    console.log('Redis error: ' + error);
-  });
-
-  return new RSVP.Promise(function(resolve, reject) {
-    client.on('connect', function() {
-      console.log('Connected to redis');
-      resolve();
-    });
-  });
-}
-
-function get(key) {
-  return new RSVP.Promise(function(resolve, reject) {
-    client.get(key, function(error, data) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    });
-  })
-};
-
